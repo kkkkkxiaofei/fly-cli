b7d8e5a71cb8ccc4870fd9feeab4276bb6eb6e7a
Xiaofei ZHANG <xiaofei.zhang@rea-group.com>
2021-05-23 13:28:43 +0800

    being able to replace keywords in template

--- utils/shell.js
+++ utils/shell.js
@@ -2,6 +2,13 @@ const { execSync } = require('child_process');
 
 const cp = (source, target = '.') => execSync(`cp -rf ${source} ${target}`);
 
+const replace = (replaceMap, inputFile) => {
+  const replaceStr = Object.entries(replaceMap).map(([key, value]) => `s/\\$${key}/${value}/g`).join(';  ');
+  // $old -> new
+  return execSync(`sed -i '' '${replaceStr}' ${inputFile}/*`);
+}
+
 module.exports = {
-  cp
+  cp,
+  replace
 };
