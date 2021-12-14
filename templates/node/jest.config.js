a863cb4c7beda35ccb7749bf3f4642f743cdc25f
Xiaofei ZHANG <xiaofei.zhang@rea-group.com>
2021-12-14 21:19:21 +0800

    modify version

--- templates/node/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/*.spec.ts'],
-  roots: ['<rootDir>/src'],
-  watchPathIgnorePatterns: ['<rootDir>/node_modules/'],
-  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
-};
