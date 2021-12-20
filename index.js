b828b122e051ed742c7e39c968d9341936a2d07d
pursue <348564768@qq.com>
2021-12-20 21:27:50 +0800

    prompt project name and project description

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
 const commander = require('commander');
 const { exists } = require('./utils/fs');
 const { cp, replace } = require('./utils/shell');
+const { startQuestion } = require('./utils/question');
 
 const program = new commander.Command();
 
 program
-  .arguments('[project]', 'project with tech set: node-web | lambda')
-  .arguments('[name]', 'project name')
+  .arguments('[service]', 'select one service from: node-web | lambda')
   .option('-d, --deploy', 'deploy to the certain cloud(aws)')
   .option('-i, --init', 'initialize your project')
   .option('-f, --force', `this option will initilize your project and automatically depoy the scaffold to cloud, hence please make sure you need this indeed
@@ -19,19 +19,21 @@ program.parse(process.argv);
 
 const { init } = program.opts();
 
-const [project, name] = program.args;
+const [service] = program.args;
 
 if (init) {
-  const templatePath = `${__dirname}/templates/${project}`;
+  const templatePath = `${__dirname}/templates/${service}`;
   const templateExisted = exists(templatePath);
   if (!templateExisted) {
-    throw new Error(`Invalid project type: ${project}, please selecte one of "node-web | lamdba"`)
+    throw new Error(`Invalid service type: ${service}, select one service from: node-web | lambda`)
   }
+  startQuestion(({ name, description }) => {
+    const cpResult = cp(`${templatePath}/`, `./${name}`);
+    console.log('=====finish cp====', cpResult);
 
-  const cpResult = cp(`${templatePath}/`, `./${name}`);
-  console.log('=====finish cp====', cpResult);
+    const replaceResult = replace({ name, desc: description }, `${process.cwd()}/${name}`);
 
-  const replaceResult = replace({ name, desc: `This is the description of ${name}` }, `${process.cwd()}/${name}`);
-
-  console.log('=====finish replace====', replaceResult);
+    console.log('=====finish replace====', replaceResult);
+  });
+  
 } 
